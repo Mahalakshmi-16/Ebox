aeac9e755494b8509e23a2997eea845440339002
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-09-24 16:01:08 +0530

    Add files via upload

---
+++ TraverseDemo.java
@@ -0,0 +1,13 @@
+package com.hcl.ebox;
+
+public class TraverseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int[] array=new int[] {12,17,19,20};
+				for(int i:array)
+				{
+					System.out.println(i);
+				}
+			}
+}
