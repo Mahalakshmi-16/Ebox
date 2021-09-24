a44c4fc68a8c814575773925ac35f71090002079
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-09-24 16:26:59 +0530

    Add files via upload

---
+++ CallByRef.java
@@ -0,0 +1,17 @@
+package com.hcl.ebox;
+
+public class CallByRef {
+		// TODO Auto-generated method stub
+		public static int display()
+		{
+			int a=1;
+			int b=2;
+			return a+b;	
+		}
+		public static void main(String[] args) 
+		{
+		int a=10;
+		int b=15;
+		System.out.println("SUM : "+ (a+b));
+		}
+}
