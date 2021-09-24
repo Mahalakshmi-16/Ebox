3cfa10c40a7217ae9fc6505187f20b7f947c1031
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-09-24 15:58:12 +0530

    Add files via upload

---
+++ ReverseDemo.java
@@ -0,0 +1,15 @@
+package com.hcl.ebox;
+
+public class ReverseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr=new int[] {1,2,3,4,5};
+		for(int i=arr.length-1;i>=0;i--)
+		{
+		System.out.println("Reverse : "+arr[i]);
+		}
+
+	}
+
+}
