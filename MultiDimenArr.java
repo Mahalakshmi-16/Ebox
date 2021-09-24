9d3cfc519cf0894cf62036174783ae3b7fb3543a
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-09-24 15:57:27 +0530

    Add files via upload

---
+++ MultiDimenArr.java
@@ -0,0 +1,20 @@
+package com.hcl.ebox;
+
+public class MultiDimenArr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]={{12,17},{4,5}};
+		for(int row=0;row<2;row++)
+		{
+			for(int col=0;col<2;col++)
+			{
+				System.out.print(arr[row][col]+" "+ " ");
+				
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
