5c4886978da8697ec003ca5eec18785d974855da
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-10-01 14:41:15 +0530

    Add files via upload

---
+++ ThreeFive.java
@@ -0,0 +1,21 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+package com.hcl;
+import java.util.*;
+public class ThreeFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<1000;i++)
+		{
+	    if(i%3==0 || i%5==0)
+		sum=sum+i;
+		}
+		System.out.println("sum of these multiples is: "+sum);
+		
+	}
+}
