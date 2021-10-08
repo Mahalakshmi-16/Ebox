e1a22808c27e44e502137d1042d0511d49aafc22
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-10-08 21:17:32 +0530

    Add files via upload

---
+++ SmaMul.java
@@ -0,0 +1,26 @@
+//5
+package com.hcl;
+public class SmaMul {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        System.out.println(mul());
+	    }
+	    public static int mul() {
+	        int i = 2520;
+	        boolean found = false;
+	        while (!found) {
+	            i=i+2520;
+	            boolean isDividable = true;
+	            for (int j = 11; j <= 20; j++) {
+	                if (i % j != 0) {
+	                    isDividable = false;
+	                    break;
+	                }
+	            }
+	            if (isDividable) {
+	                found = true;
+	            }
+	        }
+	        return i;
+	    }
+	}
