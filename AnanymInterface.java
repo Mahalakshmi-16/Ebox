83500d9a4c65484da607c55ed8ec56387f85beba
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-10-13 22:25:34 +0530

    Add files via upload

---
+++ AnanymInterface.java
@@ -0,0 +1,13 @@
+package com.hcl.ebox;
+
+public class AnanymInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+IndoorGame i=new IndoorGame();
+i.msg();
+i.c.play();
+	}
+}
+
+
