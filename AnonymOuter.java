7fd41b09ba6696cf29bb9fd37d3b98d1007ff864
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-10-13 22:28:57 +0530

    Add files via upload

---
+++ AnonymOuter.java
@@ -0,0 +1,12 @@
+package com.hcl.ebox;
+
+public class AnonymOuter {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+public void display()
+{
+	System.out.println("parent class");
+	}
+
+}
