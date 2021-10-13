fc761c7a7a4a522adf1aaa98efd4fe3ea9c55ceb
Mahalakshmi-16 <91301392+Mahalakshmi-16@users.noreply.github.com>
2021-10-13 22:26:02 +0530

    Add files via upload

---
+++ IndoorGame.java
@@ -0,0 +1,20 @@
+package com.hcl.ebox;
+
+public class IndoorGame {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Chess c=new Chess()
+{
+	public void play()
+	{
+		System.out.println("playing");
+	}
+};
+public void msg()
+{
+	c.play();
+}
+}
+
+
